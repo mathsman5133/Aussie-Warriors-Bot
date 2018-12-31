7f85fd69a81ebdf5f098dcf7e1cbae79050341fc
mathsman5133 <mathsman5132@gmail.com>
2018-12-31 21:54:15 +1100

    tidy up code, add some comments

--- bot.py
+++ bot.py
@@ -1,8 +1,5 @@
-import discord
 from discord.ext import commands
 import sys
-import datetime
-import traceback
 import json
 import os
 import functools
@@ -20,7 +17,6 @@ from cogs.utils import context
 
 json_location = os.path.join(os.getcwd(), 'creds.json')
 REPO_PATH = os.path.join(os.getcwd())
-BOT_TOKEN = ''
 
 initial_extensions = [
                       'cogs.claim',
@@ -40,6 +36,7 @@ def run_bot():
     loop = asyncio.get_event_loop()
 
     try:
+        # configure the database connection
         pool = loop.run_until_complete(Table.create_pool(creds['postgresql'], command_timeout=60))
     except Exception as e:
         click.echo('Could not set up PostgreSQL. Exiting.', file=sys.stderr)
@@ -61,51 +58,48 @@ class AWBot(commands.Bot):
             except Exception as er:
                 print(f'Failed to load extension {e}: {er}.', file=sys.stderr)
 
+        # our json loaded creds file with tokens
+        self.loaded = creds
+
+        # aiohttp session, coc token and coc session for use with clash of clans api
         self.http_session = aiohttp.ClientSession(connector=aiohttp.TCPConnector
                                                   (resolver=aiohttp.AsyncResolver,
                                                    family=socket.AF_INET))
 
-        self.loaded = creds
         self.coc_token = self.loaded['coctoken']
         self.coc = ClashOfClans(connection=self.http_session, bot=self)
 
+        # github repo object based on main directory we're in. used for `git pull` commands
         self.repo = git.Repo(REPO_PATH)
+
+        # list of owner userids to use in owner only command check
+        self.owners = [
+            230214242618441728,
+            341026885985239051
+        ]
 
     async def on_message(self, message):
         if message.author.bot:
             return  # ignore bot messages
         await self.process_commands(message)
 
-    # async def on_command_error(self, ctx, error):
-    #     # we dont want logs for this stuff which isnt our problem
-    #     ignored = (commands.NoPrivateMessage, commands.DisabledCommand, commands.CheckFailure,
-    #                commands.CommandNotFound, commands.UserInputError, discord.Forbidden)
-    #     error = getattr(error, 'original', error)
-    #
-    #     if isinstance(error, ignored):
-    #         return
-    #
-    #     e = discord.Embed(colour=discord.Colour.red())
-    #     exc = ''.join(traceback.format_exception(type(error), error, error.__traceback__, chain=False))
-    #     e.description = f'```py\n{exc}\n```'  # format legible traceback
-    #     e.timestamp = datetime.datetime.utcnow()
-    #
-    #     await ctx.send(embed=e)
-
     async def on_command(self, ctx):
+        # make bot 'type' so impatient people know we have received the command, if it is a long computation
         await ctx.message.channel.trigger_typing()
 
     async def process_commands(self, message):
+        # we have a couple attributes to add to context, lets add them now (easy db connection etc.)
         ctx = await self.get_context(message, cls=context.Context)
 
         if ctx.command is None:
-            return
+            return  # if there's no command invoked return
 
         async with ctx.acquire():
-            await self.invoke(ctx)
+            await self.invoke(ctx)  # invoke command with our database connection
 
     async def save_json(self):
-
+        """Coroutine helper to save our json file
+        """
         def save_to_json():
             """
             Save json to the file.
@@ -118,15 +112,19 @@ class AWBot(commands.Bot):
         await self.loop.run_in_executor(None, thing)
 
     async def update_coc_token(self, new_token):
+        """Update the coc api token in the creds json file
+        """
         self.loaded['coctoken'] = new_token
         await self.save_json()
 
     async def on_ready(self):
+        """Useful for knowing when bot has connected
+        """
         print(f'Ready: {self.user} (ID: {self.user.id})')
 
     def run(self):
+        # run the bot
         try:
-            # super().run(BOT_TOKEN)
             super().run(creds['bottoken'])
         except Exception as e:
             print(e)
