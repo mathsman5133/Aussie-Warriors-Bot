444e2d5f19dd58385f4615fc7deddc919cf901db
mathsman5133 <mathsman5132@gmail.com>
2019-01-03 11:45:01 +1100

    scratch message check

--- cogs/utils/paginator.py
+++ cogs/utils/paginator.py
@@ -146,8 +146,7 @@ class MsgPag:
     def react_check(self, reaction, user):
         if (user is None) or (user.id != self.author.id):
             return False
-        if reaction.message != self.message:
-            return False
+
         for (emoji, func) in self.reaction_emojis:
             if reaction.emoji == emoji:
                 self.match = func
@@ -322,8 +321,6 @@ class EmbedPag:
 
     def react_check(self, reaction, user):
         if (user is None) or (user.id != self.author.id):
-            return False
-        if reaction.message != self.message:
             return False
 
         for (emoji, func) in self.reaction_emojis:
