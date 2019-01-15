bb1ad42672d97dd90cc1b5cd527c7c7f844ae3a6
mathsman5133 <mathsman5132@gmail.com>
2019-01-16 10:54:56 +1100

    make tz aus/sydney

--- cogs/update_donations.py
+++ cogs/update_donations.py
@@ -4,6 +4,7 @@ import asyncio
 
 import discord
 from discord.ext import commands
+import pytz
 
 from cogs.utils import checks, db
 from cogs.donations import ShowDonations
@@ -287,7 +288,7 @@ class Update:
                 await Admin(self.bot).task_stats('send_pings', False)
 
                 show_donations_class = ShowDonations(self.bot)
-                today = datetime.datetime.now()
+                today = datetime.datetime.now(pytz.timezone('Australia/Sydney'))
 
                 if today.hour == 10 and today.weekday() == 2:  # if its 7oc on tuesday
                     await show_donations_class.send_donation_pings()
