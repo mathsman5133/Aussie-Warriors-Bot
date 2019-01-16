662651540ac8ae979b674c428d045103700579f3
mathsman5133 <mathsman5132@gmail.com>
2019-01-16 11:09:24 +1100

    testing

--- cogs/update_donations.py
+++ cogs/update_donations.py
@@ -290,7 +290,7 @@ class Update:
                 show_donations_class = ShowDonations(self.bot)
                 today = datetime.datetime.now(pytz.timezone('Australia/Sydney'))
 
-                if today.hour == 10 and today.weekday() == 2:  # if its 7oc on tuesday
+                if today.hour == 11 and today.weekday() == 2:  # if its 7oc on tuesday
                     await show_donations_class.send_donation_pings()
                     await Admin(self.bot).task_stats('send_pings', True)
 
